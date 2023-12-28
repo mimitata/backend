a7231ac68397ca237ebba9e56cd6948163fd629e
mariem <mariemlabidi88@gmail.com>
2023-12-28 12:53:47 +0100

    first commit

---
+++ models/userSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: { type: String },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+});
+
+const User = mongoose.model("users", UserSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app=express()
+const dotenv=require("dotenv")
+dotenv.config()
+const cors=require('cors')
+const port=process.env.PORT || 5000
+app.use(express.json())
+app.use(cors())
+const connectDB=require("./config/connectDB")
+connectDB()
+
+app.use('/api/user',require("./routes/UserRoutes"))
+app.use('/api/admin',require("./routes/AdminRoutes"))
+
+app.listen(port,(err)=>{err? console.log(err):console.log("Server is running in port:",port)})
